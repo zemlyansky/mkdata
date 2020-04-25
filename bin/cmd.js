618b8ff39fa9b0d2fcd000c9bf512a181c86c38b
zemlyansky <anton.zemlyansky@gmail.com>
2020-04-25 13:39:07 +0300

    .

---
+++ bin/cmd.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+const make = require('../')
+const fs = require('fs')
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    dataset: 'd',
+    header: 'h',
+    nFeatures: 'f',
+    nSamples: 's',
+    noise: 'n',
+    output: 'o',
+    randomState: 'r'
+  },
+  default: {
+    dataset: 'friedman1',
+    nSamples: 100
+  }
+})
+
+const dataset = argv._[0] || argv.dataset
+
+if (Object.keys(make).includes(dataset)) {
+  const [X, y] = make[argv.dataset](argv)
+  let res = X[0].map((_, i) => 'x' + i).join(',')
+  res += y ? ',y' : ''
+  res += '\n'
+
+  X.forEach((x, i) => {
+    res += x.join(',')
+    res += y ? ',' + y[i] : ''
+    res += '\n'
+  })
+
+  if (argv.output && argv.output.length) {
+    fs.writeFileSync(argv.output, res)
+  } else {
+    process.stdout.write(res)
+  }
+} else {
+  throw new Error('No dataset with a name', argv.dataset)
+}
+// Read
---
+++ index.js
@@ -0,0 +1,157 @@
+const Random = require('seedrandom')
+
+const defaults = {
+  nSamples: 100,
+  noise: 0,
+  randomState: null
+}
+
+function initRandom (seed) {
+  return seed ? new Random(seed) : Math.random
+}
+
+// Based on https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
+function normal (random) {
+  let u = 0
+  let v = 0
+  while (u === 0) u = random()
+  while (v === 0) v = random()
+  return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+}
+
+function friedman1 (opts) {
+  const datasetDefaults = {
+    nFeatures: 10
+  }
+
+  const options = Object.assign({}, defaults, datasetDefaults, opts)
+  const random = initRandom(options.seed)
+
+  if (options.nFeatures < 5) throw new Error('nFeatures must be at least five')
+
+  const X = []
+  const y = []
+
+  for (let ri = 0; ri < options.nSamples; ri++) {
+    const x = []
+    for (let ci = 0; ci < options.nFeatures; ci++) {
+      x.push(random())
+    }
+    X.push(x)
+    y.push(10 * Math.sin(Math.PI * x[0] * x[1]) + 20 * Math.pow((x[2] - 0.5), 2) + 10 * x[3] + 5 * x[4] + options.noise * random())
+  }
+
+  return [X, y]
+}
+
+function friedman2 (opts) {
+  const options = Object.assign({}, defaults, opts)
+  const random = initRandom(options.seed)
+
+  const X = []
+  const y = []
+
+  for (let ri = 0; ri < options.nSamples; ri++) {
+    const x = [
+      random() * 100,
+      random() * 520 * Math.PI + 40 * Math.PI,
+      random(),
+      random() * 10 + 1
+    ]
+    X.push(x)
+    y.push(Math.pow(x[0], 2) + Math.sqrt(Math.pow(x[1] * x[2] - 1 / (x[1] * x[3]))) + options.noise * random())
+  }
+
+  return [X, y]
+}
+
+function friedman3 (opts) {
+  const options = Object.assign({}, defaults, opts)
+  const random = initRandom(options.seed)
+
+  const X = []
+  const y = []
+
+  for (let ri = 0; ri < options.nSamples; ri++) {
+    const x = [
+      random() * 100,
+      random() * 520 * Math.PI + 40 * Math.PI,
+      random(),
+      random() * 10 + 1
+    ]
+    X.push(x)
+    y.push(Math.atan(x[1] * x[2] - 1 / (x[1] * x[3]) / x[0]) + options.noise * random())
+  }
+
+  return [X, y]
+}
+
+function hastie (opts) {
+  const options = Object.assign({}, defaults, opts)
+  const random = initRandom(options.seed)
+
+  const X = []
+  const y = []
+
+  for (let ri = 0; ri < options.nSamples; ri++) {
+    const x = []
+    let target = 0
+    for (let ci = 0; ci < 10; ci++) {
+      const n = normal(random)
+      x.push(n)
+      target += n * n
+    }
+    X.push(x)
+    y.push(+(target > 9.34))
+  }
+
+  return [X, y]
+}
+
+function moons (opts) {
+  const datasetDefaults = {
+    shuffle: 10
+  }
+
+  const options = Object.assign({}, defaults, datasetDefaults, opts)
+
+  let nSamplesIn
+  let nSamplesOut
+
+  if (Array.isArray(options.nSamples)) {
+    nSamplesOut = options.nSamples[0]
+    nSamplesIn = options.nSamples[1]
+  } else {
+    nSamplesOut = Math.floor(options.nSamples / 2)
+    nSamplesIn = options.nSamples - nSamplesOut
+  }
+
+  const X = []
+  const y = []
+
+  const stepOut = Math.PI / nSamplesOut
+  for (let s = 0; s < Math.PI; s += stepOut) {
+    X.push([Math.cos(s), Math.sin(s)])
+    y.push(0)
+  }
+
+  const stepIn = Math.PI / nSamplesIn
+  for (let s = 0; s < Math.PI; s += stepIn) {
+    X.push([0.5 - Math.cos(s), 1 - Math.sin(s)])
+    y.push(1)
+  }
+
+  if (options.shuffle) {
+    for (let ri = options.nSamples - 1; ri > 0; ri--) {
+      const i = Math.floor(Math.random() * (ri + 1))
+      ;[X[ri], X[i]] = [X[i], X[ri]]
+      ;[y[ri], y[i]] = [y[i], y[ri]]
+    }
+  }
+
+  return [X, y]
+}
+
+module.exports = {
+  friedman1, friedman2, friedman3, hastie, moons
+}
